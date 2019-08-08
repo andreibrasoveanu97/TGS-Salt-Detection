d9d1ed379f0677af1af60272fc789875d38d3c2c
andreibrasoveanu97 <32870957+andreibrasoveanu97@users.noreply.github.com>
2019-08-08 16:54:53 +0300

    some losses

--- tgs_env/losers.py
+++ tgs_env/losers.py
@@ -1,4 +1,12 @@
 import tensorflow as tf
+from keras import backend as K
 
+def dice_loss(y_true, y_pred):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = y_true_f * y_pred_f
+    score = (K.sum(y_true_f) + K.sum(y_pred_f)) / (2. * K.sum(intersection))
+    return score
 
-
+def combine_loss(y_true, y_pred):
+    return 0.85*dice_loss(y_true, y_pred) + 0.15*tf.keras.losses.binary_crossentropy(y_true, y_pred)
--- tgs_env/metrics.py
+++ tgs_env/metrics.py
@@ -1,9 +1,11 @@
 import tensorflow as tf
 from keras import backend as K
 
-
 def dice_coef(y_true, y_pred):
-    intersection = K.sum(y_true.flatten() * y_pred.flatten(), axis=[1, 2, 3])
-    union = K.sum(y_true, axis=[1, 2, 3]) + K.sum(y_pred, axis=[1, 2, 3])
-    return K.mean((2. * intersection + 1) / (union + 1), axis=0)
+    y_true_f = K.flatten(y_true)
+    y_pred = K.cast(y_pred, 'float32')
+    y_pred_f = K.cast(K.greater(K.flatten(y_pred), 0.5), 'float32')
+    intersection = y_true_f * y_pred_f
+    score = 2. * K.sum(intersection) / (K.sum(y_true_f) + K.sum(y_pred_f))
+    return score
 
