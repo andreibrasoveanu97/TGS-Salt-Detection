3e69760896b597f2e65f18ee24a5cd08b9457cbe
andreibrasoveanu97 <32870957+andreibrasoveanu97@users.noreply.github.com>
2019-08-12 10:32:13 +0300

    Update augs.py

--- tgs_env/tgs/augs.py
+++ tgs_env/tgs/augs.py
@@ -7,16 +7,11 @@ def flip(image_set, mask_set):
     imgs, masks = aug.augment_images(image_set, mask_set)
     return imgs, masks
 
-def rotate(image_set, mask_set, degrees = 90):
-    aug = iaa.Affine(rotate=degrees)
+def get_rotate(degrees = 90):
 
-    imgs = aug.augment_images(image_set)
-    masks = aug.augment_images(mask_set)
-    return imgs, masks
-
-def scale(image_set, mask_set):
-    aug = iaa.Affine(scale=1.2674)
-
-    imgs = aug.augment_images(image_set)
-    masks = aug.augment_images(mask_set)
-    return imgs, masks
+    def rotate(image_set, mask_set):
+        aug = iaa.Affine(rotate=degrees)
+        imgs = aug.augment_images(image_set)
+        masks = aug.augment_images(mask_set)
+        return imgs, masks
+    return rotate
