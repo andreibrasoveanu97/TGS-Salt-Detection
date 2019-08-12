8eae87d69c93307b8b13925740106c70efa1fe88
andreibrasoveanu97 <32870957+andreibrasoveanu97@users.noreply.github.com>
2019-08-12 10:41:45 +0300

    Update augs.py
    aug rotation done

--- tgs_env/tgs/augs.py
+++ tgs_env/tgs/augs.py
@@ -1,13 +1,14 @@
-import imgaug as ia
 import imgaug.augmenters as iaa
-from helpers import reshape_img
+
+
 def flip(image_set, mask_set):
     aug = iaa.Fliplr(1)
 
     imgs, masks = aug.augment_images(image_set, mask_set)
     return imgs, masks
 
-def get_rotate(degrees = 90):
+
+def get_rotate(degrees=90):
 
     def rotate(image_set, mask_set):
         aug = iaa.Affine(rotate=degrees)
@@ -15,3 +16,16 @@ def get_rotate(degrees = 90):
         masks = aug.augment_images(mask_set)
         return imgs, masks
     return rotate
+
+
+def get_rotate_both(degrees=90):
+
+    def rotate_both(image_set, mask_set):
+        imgs1, masks1 = get_rotate(degrees)(image_set, mask_set)
+        imgs2, masks2 = get_rotate(-degrees)(image_set, mask_set)
+        image_set.extend(imgs1)
+        image_set.extend(imgs2)
+        mask_set.extend(masks1)
+        mask_set.extend(masks2)
+        return image_set, mask_set
+    return rotate_both
