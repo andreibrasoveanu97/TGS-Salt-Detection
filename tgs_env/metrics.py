1cc8841e57a9dfa1c5c4da6d2206f2368f626dbf
andreibrasoveanu97 <32870957+andreibrasoveanu97@users.noreply.github.com>
2019-08-19 15:38:44 +0300

    model ResNet

--- tgs_env/metrics.py
+++ tgs_env/metrics.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 from keras import backend as K
+import numpy as np
+
 
 def dice_coef(y_true, y_pred):
     y_true_f = K.flatten(y_true)
@@ -9,3 +11,28 @@ def dice_coef(y_true, y_pred):
     score = 2. * K.sum(intersection) / (K.sum(y_true_f) + K.sum(y_pred_f))
     return score
 
+
+def get_iou_vector(A, B):
+    batch_size = A.shape[0]
+    metric = []
+    for batch in range(batch_size):
+        t, p = A[batch] > 0, B[batch] > 0
+
+        intersection = np.logical_and(t, p)
+        union = np.logical_or(t, p)
+        iou = (np.sum(intersection > 0) + 1e-10) / (np.sum(union > 0) + 1e-10)
+        thresholds = np.arange(0.5, 1, 0.05)
+        s = []
+        for thresh in thresholds:
+            s.append(iou > thresh)
+        metric.append(np.mean(s))
+
+    return np.mean(metric)
+
+
+def my_iou_metric(label, pred):
+    return tf.py_func(get_iou_vector, [label, pred > 0.5], tf.float64)
+
+
+def my_iou_metric_2(label, pred):
+    return tf.py_func(get_iou_vector, [label, pred > 0], tf.float64)
--- tgs_env/models.py
+++ tgs_env/models.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 import cv2
 # import tensorflow.contrib.eager as tfe
 import numpy as np
+
 from layers import UnetDecodeLayer, UnetMiddleLayer, UnetEncodeLayer
 
 
@@ -144,6 +145,9 @@ def build_model(input_layer, start_neurons, set_dropout=True):
 
     return output_layer
 
+
+
+
 def build_model_1layer(input_layer, start_neurons, set_dropout=True):
     # 128 -> 64
     conv1 = tf.keras.layers.Conv2D(start_neurons * 1, (3, 3), activation="relu", padding="same")(input_layer)
@@ -206,6 +210,107 @@ def build_model_1layer(input_layer, start_neurons, set_dropout=True):
 
     return output_layer
 
+
+def batchActivate(x):
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.Activation('relu')(x)
+    return x
+
+
+def convolution_block(x, filters, size, strides=(1,1), padding='same', activation=True):
+    x = tf.keras.layers.Conv2D(filters, size, strides=strides, padding=padding)(x)
+    if activation:
+        x = batchActivate(x)
+    return x
+
+
+def residual_block(blockInput, num_filters=16, batch_activate = False):
+    x = batchActivate(blockInput)
+    x = convolution_block(x, num_filters, (3,3) )
+    x = convolution_block(x, num_filters, (3,3), activation=False)
+    x = tf.keras.layers.Add()([x, blockInput])
+    if batch_activate:
+        x = batchActivate(x)
+    return x
+
+
+def build_model_resnet(input_layer, start_neurons, DropoutRatio = 0.5):
+    conv1 = tf.keras.layers.Conv2D(start_neurons * 1, (3, 3), activation=None, padding="same")(input_layer)
+    conv1 = residual_block(conv1, start_neurons * 1)
+    conv1 = residual_block(conv1, start_neurons * 1, True)
+    pool1 = tf.keras.layers.MaxPooling2D((2, 2))(conv1)
+    pool1 = tf.keras.layers.Dropout(DropoutRatio / 2)(pool1)
+
+    # 50 -> 25
+    conv2 = tf.keras.layers.Conv2D(start_neurons * 2, (3, 3), activation=None, padding="same")(pool1)
+    conv2 = residual_block(conv2, start_neurons * 2)
+    conv2 = residual_block(conv2, start_neurons * 2, True)
+    pool2 = tf.keras.layers.MaxPooling2D((2, 2))(conv2)
+    pool2 = tf.keras.layers.Dropout(DropoutRatio)(pool2)
+
+    # 25 -> 12
+    conv3 = tf.keras.layers.Conv2D(start_neurons * 4, (3, 3), activation=None, padding="same")(pool2)
+    conv3 = residual_block(conv3, start_neurons * 4)
+    conv3 = residual_block(conv3, start_neurons * 4, True)
+    pool3 = tf.keras.layers.MaxPooling2D((2, 2))(conv3)
+    pool3 = tf.keras.layers.Dropout(DropoutRatio)(pool3)
+
+    # 12 -> 6
+    conv4 = tf.keras.layers.Conv2D(start_neurons * 8, (3, 3), activation=None, padding="same")(pool3)
+    conv4 = residual_block(conv4, start_neurons * 8)
+    conv4 = residual_block(conv4, start_neurons * 8, True)
+    pool4 = tf.keras.layers.MaxPooling2D((2, 2))(conv4)
+    pool4 = tf.keras.layers.Dropout(DropoutRatio)(pool4)
+
+    # Middle
+    convm = tf.keras.layers.Conv2D(start_neurons * 16, (3, 3), activation=None, padding="same")(pool4)
+    convm = residual_block(convm, start_neurons * 16)
+    convm = residual_block(convm, start_neurons * 16, True)
+
+    # 6 -> 12
+    deconv4 = tf.keras.layers.Conv2DTranspose(start_neurons * 8, (3, 3), strides=(2, 2), padding="same")(convm)
+    uconv4 = tf.keras.layers.concatenate([deconv4, conv4])
+    uconv4 = tf.keras.layers.Dropout(DropoutRatio)(uconv4)
+
+    uconv4 = tf.keras.layers.Conv2D(start_neurons * 8, (3, 3), activation=None, padding="same")(uconv4)
+    uconv4 = residual_block(uconv4, start_neurons * 8)
+    uconv4 = residual_block(uconv4, start_neurons * 8, True)
+
+    # 12 -> 25
+    # deconv3 = Conv2DTranspose(start_neurons * 4, (3, 3), strides=(2, 2), padding="same")(uconv4)
+    deconv3 = tf.keras.layers.Conv2DTranspose(start_neurons * 4, (3, 3), strides=(2, 2), padding="valid")(uconv4)
+    uconv3 = tf.keras.layers.concatenate([deconv3, conv3])
+    uconv3 = tf.keras.layers.Dropout(DropoutRatio)(uconv3)
+
+    uconv3 = tf.keras.layers.Conv2D(start_neurons * 4, (3, 3), activation=None, padding="same")(uconv3)
+    uconv3 = residual_block(uconv3, start_neurons * 4)
+    uconv3 = residual_block(uconv3, start_neurons * 4, True)
+
+    # 25 -> 50
+    deconv2 = tf.keras.layers.Conv2DTranspose(start_neurons * 2, (3, 3), strides=(2, 2), padding="same")(uconv3)
+    uconv2 = tf.keras.layers.concatenate([deconv2, conv2])
+
+    uconv2 = tf.keras.layers.Dropout(DropoutRatio)(uconv2)
+    uconv2 = tf.keras.layers.Conv2D(start_neurons * 2, (3, 3), activation=None, padding="same")(uconv2)
+    uconv2 = residual_block(uconv2, start_neurons * 2)
+    uconv2 = residual_block(uconv2, start_neurons * 2, True)
+
+    # 50 -> 101
+    # deconv1 = Conv2DTranspose(start_neurons * 1, (3, 3), strides=(2, 2), padding="same")(uconv2)
+    deconv1 = tf.keras.layers.Conv2DTranspose(start_neurons * 1, (3, 3), strides=(2, 2), padding="valid")(uconv2)
+    uconv1 = tf.keras.layers.concatenate([deconv1, conv1])
+
+    uconv1 = tf.keras.layers.Dropout(DropoutRatio)(uconv1)
+    uconv1 = tf.keras.layers.Conv2D(start_neurons * 1, (3, 3), activation=None, padding="same")(uconv1)
+    uconv1 = residual_block(uconv1, start_neurons * 1)
+    uconv1 = residual_block(uconv1, start_neurons * 1, True)
+
+    # uconv1 = Dropout(DropoutRatio/2)(uconv1)
+    # output_layer = Conv2D(1, (1,1), padding="same", activation="sigmoid")(uconv1)
+    output_layer_noActi = tf.keras.layers.Conv2D(1, (1, 1), padding="same", activation=None)(uconv1)
+    output_layer = tf.keras.layers.Activation('sigmoid')(output_layer_noActi)
+
+    return output_layer
 
 if __name__ == '__main__':
     model = UnetModel()
